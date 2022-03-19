ac7a7afef0eac836a3153edfa1574013145496d6
Joannarama <joanne@bolgers.net>
2022-03-19 12:29:11 +0000

    construct basic game flow, initial selcetion of rules or game

--- run.py
+++ run.py
@@ -1,4 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Hello World!")
+def start_game():
+    """
+    User selects an option to begin or see rules
+    """
+    print("Welcome to Hangman")
+    print("Please select option 1 or 2 to start")
+
+    print("1. Game Rules")
+    print("2. Play the Game")
+
+    choice = input("Press 1 or 2 and press enter: ")
+
+    if (choice == "1"):
+        game_rules()
+    elif (choice == "2"):
+        play_game()    
+    else:
+        print("Please make a valid selection: 1 or 2")    
+        start_game()
+
+def game_rules():
+    """
+    Game Rules
+    """
+    print("game rules")
+
+def play_game():
+    """
+    Game
+    """
+    print("play game")    
+
+def user_guess():
+    """
+    Handles the user guess
+    """
+    guess = input("Guess a Letter: ")
+    # validate the letter selection by the user
+    print(f"The letter you guessed is {guess}")
+
+start_game()
+
