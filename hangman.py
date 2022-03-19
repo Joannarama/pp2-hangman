32ad4f0df22482f488f140f1c9714ee9a903edad
Joannarama <joanne@bolgers.net>
2022-03-19 18:54:33 +0000

    fixed bugs with upper/lowercase letters guessed and fixed tries countdown

--- hangman.py
+++ hangman.py
@@ -5,46 +5,82 @@ from game_data import hangman_level
 class Hangman:
     """This is the class for handling the hangman gameplay"""
 
+    def __init__(self):
+        # number of tries a user has
+        self.number_of_tries = 8
+
+        # variable for number of guesses
+        self.number_of_errors = 0
+
+        # letters the user has tried
+        self.letters_used = []
+
+        # variable for our word or words
+        self.word = Word()
+
     def play_game(self):
         """
         Game
         """
-
-        # number of tries a user has
-        number_of_tries = 8
-
-        # variable for number of guesses
-        number_of_errors = 0
-
-        # variable for our word or words
-        word = Word()
-
         while True:
-            word.print_the_word()
+            self.word.print_the_word(self.letters_used)
 
-            self.user_guess()
-            number_of_errors += 1
+            guess = self.user_guess()
 
-            self.draw_hangman(number_of_errors)
+            if guess is not False:
+                self.draw_hangman()
 
-            print(f"number of tires {number_of_tries}")
-            if number_of_errors > number_of_tries:
-                self.game_over('failed', word.game_word)
+                tries_left = self.number_of_tries - self.number_of_errors
+
+                print(f"number of tries left: {tries_left}")
+
+                letters_used_string = ' '.join(map(str, self.letters_used))
+                print(f"The letters used {letters_used_string}")
+
+                if tries_left == 0:
+                    self.game_over('failed', self.word.game_word)
 
     def user_guess(self):
         """
         Handles the user guess
         """
-        guess = input("Guess a Letter: ")
-        # validate the letter selection by the user
-        print(f"The letter you guessed is {guess}")
+        self.word.guess = ''
 
-    def draw_hangman(self, error):
+        guess = input("Guess a Letter: ")
+
+        guess = guess.lower()
+
+        if self.validate_guess(guess) is False:
+            print(
+                "You have entered an incorrect character, enter only letters"
+                )
+            return False
+        elif self.letter_already_used(guess):
+            print("You have already used this letter")
+            return False
+        else:
+            print(f"The letter you guessed is {guess}")
+
+            self.word.guess = guess
+
+            self.letters_used.append(guess)
+
+            if self.word.is_letter_in_word(guess) is False:
+                self.number_of_errors += 1
+                print(f"The letter {guess} is not in the word!")
+            else:
+                print(f"The letter {guess} is in the word!")
+
+            return guess
+
+    def draw_hangman(self):
         """
         Draws the hangman
         """
-        error -= 1       
-        print(hangman_level[error])
+        if (self.number_of_errors == 0):
+            print(hangman_level[0])
+        else:
+            print(hangman_level[self.number_of_errors])
 
     def game_over(self, result, word):
         """
@@ -57,4 +93,27 @@ class Hangman:
             print("You lost!")
             print("The correct answer was: " + word)
             input("Press enter to try again")
+            self.reset_game()
             self.play_game()
+
+    def validate_guess(self, guess):
+        """
+        Validate the user input
+        """
+        if guess.isalpha():
+            return True
+        else:
+            return False
+
+    def letter_already_used(self, guess):
+        """
+        Check if the letter has already been used
+        """
+        if guess in self.letters_used:
+            return True
+        else:
+            return False
+
+    def reset_game(self):
+        """ Reset the game """
+        self.word.get_word()
--- word.py
+++ word.py
@@ -7,22 +7,22 @@ class Word:
 
     def __init__(self):
         self.game_word = self.get_word()
+        self.guess = ''
 
     def get_word(self):
         """
         get the word for the user to use
         """
+        return 'Oasis'
         category = random.choice(list(game_data.values()))
         return random.choice(category)
 
-    def print_the_word(self):
+    def print_the_word(self, letters_used):
         """
         Prints the word for the user, if the user has guessed a letter show
         that letter, otherwise show an underscore
         """
         word_split = self.split_word_into_characters()
-
-        guessed_letter = 'T'
 
         word_output = ''
 
@@ -30,12 +30,23 @@ class Word:
             # if  blank space add blank space
             if letter == ' ':
                 word_output += '  '
-            elif letter == guessed_letter:
-                word_output += guessed_letter + ' '
+            elif letter.lower() == self.guess:
+                word_output += letter + ' '
+            elif letter.lower() in letters_used:
+                word_output += letter + ' '
             else:
                 word_output += '_ '
 
         print(word_output.strip() + "\n")
+
+    def is_letter_in_word(self, guess):
+        """ Check if a letter is in the game word """
+        lower_case_game_word = [x.lower() for x in self.game_word]
+
+        if guess.lower() in lower_case_game_word:
+            return True
+        else:
+            return False
 
     def split_word_into_characters(self):
         """
